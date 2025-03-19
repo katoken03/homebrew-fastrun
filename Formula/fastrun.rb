cdb5d460238744d4a9305c22895a840f2fba12d9
akat03 <kato@econosys-system.com>
2025-03-19 21:39:09 +0900

    Update fastrun to v0.3.1

--- Formula/fastrun.rb
+++ Formula/fastrun.rb
@@ -2,9 +2,9 @@ class Fastrun < Formula
   desc "Fast command launcher with fuzzy search for npm scripts and make targets"
   homepage "https://github.com/katoken03/fastrun"
   
-  url "https://github.com/katoken03/fastrun/archive/v0.3.0.tar.gz"
-  version "0.3.0"
-  sha256 "bb135f5d66ea2468ffe97b476b9c32ee7d4a23b8936a88d0f48a27f3b0a2531b"
+  url "https://github.com/katoken03/fastrun/archive/v0.3.1.tar.gz"
+  version "0.3.1"
+  sha256 "9fb9ff5988b2494d0833f5c8cf03ebb25f573e13605d7bb60bbd7c056a0135ef"
   license "MIT"
 
   depends_on "go" => :build
@@ -21,6 +21,11 @@ class Fastrun < Formula
       1. Move to a directory with package.json or Makefile
       2. Just type 'f' and press Enter
          f [Enter]
+
+      For proper shell history integration, run:
+         f generate-shell-function --shell=bash  # For bash users
+         f generate-shell-function --shell=zsh   # For zsh users
+      And follow the instructions to add the function to your shell config.
     EOS
   end
 
